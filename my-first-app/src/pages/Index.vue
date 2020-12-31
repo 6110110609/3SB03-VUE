516774febaded4c306103691b84dc4c7dc85381d
6110110609 <6110110609@psu.ac.th>
2020-12-31 22:32:38 +0700

    first commit

---
+++ my-first-app/src/pages/Index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="q-pa-md">
+    <q-carousel
+      animated
+      v-model="slide"
+      arrows
+      navigation
+      infinite
+    >
+      <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+      <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+      <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+      <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+    </q-carousel>
+    <div class="column items-center q-pa-md q-gutter-sm" style="height: 150px">
+      <q-btn icon="home" label="AboutMe" @click="$router.replace('/AboutMe')" glossy color="purple" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      slide: 1
+    }
+  }
+}
+</script>
